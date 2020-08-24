85891c5c34a92f8e17c72b912484a054765c0b75
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-08-24 02:20:51 -0300

    docs(backend): add swagger documentation to the backend api

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('NestJs backend starter')
+      .setDescription('Backend starter API for simple web apps or blogs')
+      .setVersion(version)
+      .addTag('Resources')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
