4fee4e366776f88c97e85506fff159eb0aab3856
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-09-04 14:25:23 -0300

    feat(backend): add custom error messages
    I create a list of key-value thinking about a
    future internationalization.

--- packages/backend/src/users/dtos/create-user.dto.ts
+++ packages/backend/src/users/dtos/create-user.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength, MaxLength, IsOptional } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
@@ -6,14 +13,14 @@ export class CreateUserDto {
   @IsNotEmpty()
   email: string;
 
-  @MaxLength(32)
-  @MinLength(8)
+  @MaxLength(32, { message: 'pass.max.len.dto' })
+  @MinLength(8, { message: 'pass.min.len.dto' })
   @IsString()
   @IsNotEmpty()
   password: string;
 
-  @MaxLength(32)
-  @MinLength(4)
+  @MaxLength(32, { message: 'username.max.len.dto' })
+  @MinLength(4, { message: 'username.min.len.dto' })
   @IsString()
   @IsOptional()
   username?: string;
