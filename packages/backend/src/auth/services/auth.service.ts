fb8f680a8cbe92aafa31954a4cc7aeb2afceff88
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-09-04 15:06:46 -0300

    feat(backend): add auth service

--- packages/backend/src/auth/services/auth.service.ts
+++ packages/backend/src/auth/services/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { BcryptService } from '../../shared/services/bcrypt.service';
+import { UserEntity } from '../../users/entities/user.entity';
+import { UsersService } from '../../users/services/users.service';
+import { UserWithoutPassword } from '../../users/types/user-types.type';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private logger = new Logger('AuthService');
+
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly bcryptService: BcryptService,
+  ) {}
+
+  async validateUser(
+    userData: Partial<UserEntity>,
+  ): Promise<UserWithoutPassword | null> {
+    this.logger.log(JSON.stringify(userData));
+    const { password: sentPassword, ...cleanUserData } = userData;
+    const user = await this.usersService.findOne(cleanUserData);
+
+    if (user && this.bcryptService.checkPassword(sentPassword, user.password)) {
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword as UserWithoutPassword;
+    }
+
+    return null;
+  }
+}
