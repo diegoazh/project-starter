cf6e808b3c1aace311144caeca5624c09b0f9a15
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-09-04 16:48:37 -0300

    test(backend): update sanitize test

--- packages/backend/src/shared/services/crypto.service.spec.ts
+++ packages/backend/src/shared/services/crypto.service.spec.ts
@@ -1,18 +1,29 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from './crypto.service';
 
+const configServiceMock = {
+  get: jest.fn((arg) => arg),
+};
+
 describe('CryptoService', () => {
   let service: CryptoService;
+  let config: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CryptoService],
+      providers: [
+        CryptoService,
+        { provide: ConfigService, useValue: configServiceMock },
+      ],
     }).compile();
 
     service = module.get<CryptoService>(CryptoService);
+    config = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(config).toBeDefined();
   });
 });
