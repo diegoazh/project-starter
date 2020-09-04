daf7ad55bd7141335f0728909f45cf29d8cba3c3
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-09-04 15:01:16 -0300

    feat(backend): add auth module

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { SharedModule } from '../shared/shared.module';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategies/local-strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [UsersModule, SharedModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
