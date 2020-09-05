3dae91929a1885ccc0554f6dcdde46568a1d02c7
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2020-09-05 11:47:47 -0300

    fix(backend): fix sanitize auth service test

--- packages/backend/src/auth/services/auth.service.spec.ts
+++ packages/backend/src/auth/services/auth.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BcryptService } from '../../shared/services/bcrypt.service';
+import { UsersService } from '../../users/services/users.service';
 import { AuthService } from './auth.service';
+
+const usersServiceMock = {};
+const bcryptServiceMock = {};
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersService: UsersService;
+  let bcryptService: BcryptService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: usersServiceMock },
+        { provide: BcryptService, useValue: bcryptServiceMock },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    bcryptService = module.get<BcryptService>(BcryptService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+    expect(bcryptService).toBeDefined();
   });
 });
